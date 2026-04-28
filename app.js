b227e61dccad97de86856da86533992acfb21278
cauabahia72 <cauabahia72@gmail.com>
2026-04-28 16:02:17 -0300

    agora vai

--- app.js
+++ app.js
@@ -36,8 +36,8 @@ async function inserir(numero, nome, email) {
     
     alert("usuario cadastrado");
 
-    document.getElementById("nome").value("");
-    document.getElementById("email").value("");
-    document.getElementById("numero").value("");
+    document.getElementById("nome").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("numero").value = "";
 
 }
